fc43f5327cb8e912e5cd122c23b1ff9c6711d0f6
David Rio Deiros <driodeiros@gmail.com>
2025-04-15 11:17:57 -0400

    ch3

--- set1.go
+++ set1.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
+	"log"
 )
 
 // encodes a hex string into base64
@@ -16,4 +17,31 @@ func hexToBase64(data string) string {
 
 	sEnc := base64.StdEncoding.EncodeToString([]byte(decoded))
 	return sEnc
+}
+
+// run xor on a and b
+func runXOR(a string, b string) string {
+	// Step 1: hex decode and get the bytes for the inputs
+	aBytes, err := hex.DecodeString(a)
+	if err != nil {
+		log.Fatalf("Failed to decode a: %v", err)
+	}
+
+	bBytes, err := hex.DecodeString(b)
+	if err != nil {
+		log.Fatalf("Failed to decode b: %v", err)
+	}
+
+	// Step 2:
+	if len(aBytes) != len(bBytes) {
+		log.Fatalf("Inputs must be of equal length")
+	}
+
+	result := make([]byte, len(aBytes))
+	for i := 0; i < len(aBytes); i++ {
+		result[i] = aBytes[i] ^ bBytes[i]
+	}
+
+	// Step 3 (convert to hex) coming next
+	return hex.EncodeToString(result)
 }
--- set1_test.go
+++ set1_test.go
@@ -13,3 +13,14 @@ func TestHexToBase64(t *testing.T) {
 	}
 
 }
+
+func TestRunXOR(t *testing.T) {
+	input1 := "1c0111001f010100061a024b53535009181c"
+	input2 := "686974207468652062756c6c277320657965"
+	expected := "746865206b696420646f6e277420706c6179"
+
+	result := runXOR(input1, input2)
+	if result != expected {
+		t.Errorf("Expected %s, but got %s", expected, result)
+	}
+}
