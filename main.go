3886a3455b7b0db8198af97f8c8cb39d804cfc4d
David Rio Deiros <driodeiros@gmail.com>
2025-04-17 09:33:38 -0400

    set1-ch4

--- main.go
+++ main.go
@@ -1,9 +1,43 @@
 package main
 
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+)
+
 func runSet1Ch3() {
 	scoreLoop("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
 }
 
+func runSet1Ch4() {
+	file, err := os.Open("data/set1/4.txt")
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	lineNumber := 1
+	for scanner.Scan() {
+		line := scanner.Text()
+		lineNumber++
+		if err := scanner.Err(); err != nil {
+			log.Fatalf("Error reading line %d: %v", lineNumber-1, err)
+		}
+
+		line = strings.TrimSuffix(line, "\r")
+		scoreLoop(line)
+	}
+
+	// Final check for any errors that might have occurred
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error scanning file: %v", err)
+	}
+}
+
 func main() {
-	runSet1Ch3()
+	runSet1Ch4()
 }
