d5da4d69ae8f47d21613b3781b2917773b70db29
David Rio Deiros <driodeiros@gmail.com>
2025-04-25 08:36:01 -0400

    refactor

--- set1_test.go
+++ set1_test.go
@@ -1,8 +1,40 @@
 package main
 
 import (
+	"encoding/hex"
 	"testing"
 )
+
+func TestConvertToHexCh01(t *testing.T) {
+	input := `49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d`
+	expect := `SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t`
+
+	got := hexToBase64(input)
+	if got != expect {
+		t.Errorf("wrong hex to base64 for %s\ngot %s\nexpected: %s", input, got, expect)
+	}
+}
+
+func TestFixedXORCh02(t *testing.T) {
+	one := hexToBin(`1c0111001f010100061a024b53535009181c`)
+	two := hexToBin(`686974207468652062756c6c277320657965`)
+	expect := `746865206b696420646f6e277420706c6179`
+
+	got := hex.EncodeToString(runXORBytes(one, two))
+	if got != expect {
+		t.Errorf("wrong xor inputs %s %s\ngot %s\nexpected: %s", one, two, got, expect)
+	}
+}
+
+func TestSingleByteXORCipherCh03(t *testing.T) {
+	inputHex := `1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736`
+	got := scoreLoopBest(inputHex)
+	expect := `X`
+
+	if got != expect {
+		t.Errorf("wrong key for plaintext\n%s\ngot %s\nexpected: %s", inputHex, got, expect)
+	}
+}
 
 func TestComputeBlockHD(t *testing.T) {
 	// Test normal cases in a loop
